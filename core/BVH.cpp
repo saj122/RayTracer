ca0514831a6b0b94a36b8234c653e4b600d508a1
Stephen Johnson <sajttu22@gmail.com>
2021-08-22 10:58:01 -0500

    Fixed method declaration in BVH.

--- core/BVH.cpp
+++ core/BVH.cpp
@@ -4,7 +4,20 @@
 #include "HitRecord.hpp"
 
 #include <algorithm>
-#include <functional>
+
+inline bool box_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b, int axis)
+{
+	AABB box_a;
+	AABB box_b;
+
+	if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
+		std::cerr << "No bounding box in BVHNode constructor.\n";
+
+	return box_a.aabb_min()[axis] < box_b.aabb_min()[axis];
+}
+bool box_x_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 0); }
+bool box_y_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 1); }
+bool box_z_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 2); }
 
 BVHNode::BVHNode(const std::vector<std::shared_ptr<Hittable>>& src_objects, size_t start, size_t end, double time0, double time1)
 {
--- core/BVH.hpp
+++ core/BVH.hpp
@@ -10,20 +10,6 @@
 class Ray;
 class HitRecord;
 
-inline bool box_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b, int axis)
-{
-	AABB box_a;
-	AABB box_b;
-
-	if (!a->bounding_box(0, 0, box_a) || !b->bounding_box(0, 0, box_b))
-		std::cerr << "No bounding box in BVHNode constructor.\n";
-
-	return box_a.aabb_min()[axis] < box_b.aabb_min()[axis];
-}
-inline bool box_x_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 0); }
-inline bool box_y_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 1); }
-inline bool box_z_compare(const std::shared_ptr<Hittable> a, const std::shared_ptr<Hittable> b) { return box_compare(a, b, 2); }
-
 class BVHNode : public Hittable
 {	
 	public:
